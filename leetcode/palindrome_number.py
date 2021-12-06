81086382d3d80e60576cba7ca9b021f94f02233b
hophoet <hophoet@gmail.com>
2021-12-06 21:13:25 +0000

    (palindrome_number) update

--- leetcode/palindrome_number.py
+++ leetcode/palindrome_number.py
@@ -1,15 +1,11 @@
 # palindrome number it reads the same backward as forward
 
-
 def is_palindrome(x:int):
 	""" palindrome """
-	x_list = [n for n in str(x)]	
+	x_list:list = [n for n in str(x)]	
 	x_list.reverse()
-	reverse_x = int( ''.join(x_list)) 
-	return True if x == reverse_x else False
+	return True if str(x) == ''.join(x_list) else False
 
-
-#print(is_palindrome(121))
-#print(is_palindrome(123))
-
-
+# print(is_palindrome(121))
+# print(is_palindrome(-121))
+# print(is_palindrome(123))
